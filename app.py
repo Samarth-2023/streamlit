db084c31e8ee3d30db896605e9d345706a1c77ad
Samarth-2023 <143649803+Samarth-2023@users.noreply.github.com>
2023-08-31 01:27:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import streamlit as st
 import time
 st.header("Digi-Clock")
 def app_logic(hours, mins, secs):
-  while True:
-    time.sleep(1)
-    if secs == 59:
-      if mins==59:
-        hours=(hours+1)%24
-      else:
-        mins+=1
+  time.sleep(1)
+  if secs == 59:
+    if mins==59:
+      hours=(hours+1)%24
+      mins=0
     else:
-      secs+=1
-    st.subheader(f"Time : {hours}:{mins}:{secs}")
+      mins=(mins+1)
+      secs=0
+  else:
+    secs+=1
+
+  return(hours, mins, secs)    
       
-hours=st.number_input('Hour', min_value=0, max_value=23, step=1)
-mins=st.number_input('Minutes', min_value=0, max_value=59, step=1)
-secs=st.number_input('Seconds', min_value=0, max_value=59, step=1)
-app_logic(hours, mins, secs)
-  
+while True:
+  (hours, mins, secs)=app_logic(0, 0, 0)
+  st.header(f"Time : {hours}:{mins}:{secs}")
